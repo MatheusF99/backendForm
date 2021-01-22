4ef355a3f9248855bd63949f1d49b18fd5a45075
matheusF99 <mc79846@gmail.com>
2021-01-21 22:41:27 -0300

    dados inseridos

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { getRepository } from 'typeorm'
+import Usuarios from '../src/models/Usuarios'
 
 const routes = Router()
 
@@ -8,26 +9,35 @@ routes.get('/', ( req, res )=>{
         res.json({message: 'hello world'})
     )
 })
-routes.post('/usuarios', ( req, res )=>{
+routes.post('/usuarios', async( req, res )=>{
     
     const {
         name,
-
         latititude,
-
         longitude,
-
         about,
-
         instructions, 
-
         open_in_hours, 
-
         open_on_weekends,
-
         telefone,
 
     } = req.body
+
+    const usuarioRepository = getRepository(Usuarios)
+
+    const usuario = usuarioRepository.create({
+        name,
+        latititude,
+        longitude,
+        about,
+        instructions, 
+        open_in_hours, 
+        open_on_weekends,
+        telefone,
+
+    })
+
+    await usuarioRepository.save(usuario)
     
     return (
         
